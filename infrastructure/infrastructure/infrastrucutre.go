52544c336cf0ac32c072fab2984b853ba7208329
Sparsha <sparshadotel@gmail.com>
2019-09-09 09:41:39 +0545

    Update the terraform output function to accomodate backend outputs

--- infrastructure/infrastructure/infrastrucutre.go
+++ infrastructure/infrastructure/infrastrucutre.go
@@ -9,16 +9,30 @@ import (
 	"path/filepath"
 )
 
+type terraformOutput struct {
+	Sensitive bool   `json:"sensitive"`
+	Type      string `json:"type"`
+	Value     string `json:"value"`
+}
+type FrontendTerraformOutput struct {
+	BucketName     terraformOutput `json:"bucket_name"`
+	FrontendWebUrl terraformOutput `json:"frontend_web_url"`
+}
+
 func InitializeFrontend(ClientArgs []byte) (string, error) {
+	workspaceRoot := "/tmp"
 
 	var clientArgs utils.Client
+	var frontendTerraformOutput FrontendTerraformOutput
+
 	logger.LogInfo("Gathering Info")
+
 	err := json.Unmarshal(ClientArgs, &clientArgs)
 	if err != nil {
 		logger.LogError(err, "Error Parsing Body")
 		return "", err
 	}
-	workspaceDir := filepath.Join("/tmp", clientArgs.Deployment.Name)
+	workspaceDir := filepath.Join(workspaceRoot, clientArgs.Deployment.Name)
 	logger.LogInfo("Generating Template")
 	err = utils.GenerateFrontendTemplateFile(frontend_architecture.InfrastructureTemplate, clientArgs, workspaceDir)
 	if err != nil {
@@ -26,17 +40,18 @@ func InitializeFrontend(ClientArgs []byte) (string, error) {
 		return "", err
 	}
 	logger.LogInfo("Running Infrastructure Changes")
-	bucketName, url, err := terraform.RunFrontendInfrastructureChanges(workspaceDir)
+	terraformOutput, err := terraform.RunInfrastructureChanges(workspaceDir)
 	if err != nil {
 		logger.LogError(err, "Cannot Run Changes")
 		return "", err
 	}
+	_ = json.Unmarshal([]byte(terraformOutput), &frontendTerraformOutput)
 	result := utils.FrontendResult{
 		Project:    clientArgs.Project,
 		Deployment: clientArgs.Deployment,
 		Data: utils.Frontend{
-			BucketName: bucketName,
-			Url:        url,
+			BucketName: frontendTerraformOutput.BucketName.Value,
+			Url:        frontendTerraformOutput.FrontendWebUrl.Value,
 		},
 	}
 	out, err := json.Marshal(result)
--- infrastructure/internal/terraform/terraform.go
+++ infrastructure/internal/terraform/terraform.go
@@ -6,12 +6,10 @@ import (
 	"github.com/briandowns/spinner"
 	"github.com/leapfrogtechnology/shift/core/utils/logger"
 	"os/exec"
-	"strings"
 	"time"
 )
 
 // TODO use terraform Library to remove the dependency of installing terraform
-
 func initTerraform(workspaceDir string) error {
 	s := spinner.New(spinner.CharSets[11], 100*time.Millisecond) // Build our new spinner
 	s.Prefix = "  "
@@ -56,60 +54,48 @@ func applyTerraform(workspaceDir string) error {
 	return nil
 }
 
-func getTerraformOutputFrontend(workspaceDir string) (string, string, error) {
+func getTerraformOutput(workspaceDir string) (string, error) {
 	s := spinner.New(spinner.CharSets[11], 100*time.Millisecond) // Build our new spinner
 	s.Prefix = "  "
 	s.Suffix = "  Generating Output"
 	_ = s.Color("cyan", "bold")
 	s.Start()
-	cmd1 := exec.Command("terraform", "output", "bucket_name")
-	cmd1.Dir = workspaceDir
-	var stdout1 bytes.Buffer
-	var stderr1 bytes.Buffer
-	cmd1.Stdout = &stdout1
-	cmd1.Stderr = &stderr1
-	err := cmd1.Run()
+	cmd := exec.Command("terraform", "output", "-json")
+	cmd.Dir = workspaceDir
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	err := cmd.Run()
 	if err != nil {
-		logger.LogError(err, stderr1.String())
+		logger.LogError(err, stderr.String())
 		s.Stop()
-		return "", "", err
+		return "", err
 	}
-	bucketName := strings.TrimSuffix(stdout1.String(), "\n")
-	cmd2 := exec.Command("terraform", "output", "frontend_web_url")
-	cmd2.Dir = workspaceDir
-	var stdout2 bytes.Buffer
-	var stderr2 bytes.Buffer
-	cmd2.Stdout = &stdout2
-	cmd2.Stderr = &stderr2
-	err = cmd2.Run()
-	if err != nil {
-		logger.LogError(err, stderr2.String())
-		s.Stop()
-		return "", "", err
-	}
-	url := "https://" + strings.TrimSuffix(stdout2.String(), "\n")
 	s.Stop()
 
-	return bucketName, url, err
+	return stdout.String(), err
 }
 
-func RunFrontendInfrastructureChanges(workspaceDir string) (string, string, error) {
+func RunInfrastructureChanges(workspaceDir string) (string, error) {
+	//terraformCmdSequence :=[][]string {{"init"}, {"apply", "--auto-approve"}, {"output", "-json"}}
 	logger.LogInfo("Initializing")
 	err := initTerraform(workspaceDir)
 	if err != nil {
 		logger.LogError(err, "Something Went Wrong")
-		return "", "", err
+		return "", err
 	}
 	logger.LogInfo("Applying")
 	err = applyTerraform(workspaceDir)
 	if err != nil {
 		logger.LogError(err, "Something Went Wrong")
-		return "", "", err
+		return "", err
 	}
-	bucketName, url, err := getTerraformOutputFrontend(workspaceDir)
+	out, err := getTerraformOutput(workspaceDir)
 	if err != nil {
 		logger.LogError(err, "Something Went Wrong")
-		return "", "", err
+		return "", err
 	}
-	return bucketName, url, err
+
+	return out, err
 }
