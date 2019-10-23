0585a474982ec6b78ca967e9e42522ebe9040cc9
pratishshr <pratishshr@gmail.com>
2019-10-23 09:10:49 +0545

    Update message

--- cli/internals/deploy/deploy.go
+++ cli/internals/deploy/deploy.go
@@ -32,5 +32,5 @@ func Run(environment string) {
 		frontend.Deploy(project, environment)
 	}
 
-	slack.Notify(project.SlackURL, fmt.Sprintf("*%s* has been succesfully deployed to *%s*.", project.Name, environment), "#04EBB8")
+	slack.Notify(project.SlackURL, fmt.Sprintf("*%s* succesfully deployed to *%s*.", project.Name, environment), "#04EBB8")
 }
