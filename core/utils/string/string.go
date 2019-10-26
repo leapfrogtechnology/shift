e01da8924d285b1d3a9209c26cb62bd78e279702
anish manandhar <anees.manandhar@gmail.com>
2019-10-26 15:50:47 +0545

    Added test cases for string.go

--- core/utils/string/string.go
+++ core/utils/string/string.go
@@ -8,6 +8,9 @@ import (
 
 // IsAlphaNumeric : Checks if the given value is alpha numeric
 func IsAlphaNumeric(val string) bool {
+	if CheckIsEmptyOrNull(val) {
+		return false
+	}
 	re := regexp.MustCompile("^[a-zA-Z0-9_]*$")
 	res := re.MatchString(val)
 	fmt.Println(res)
@@ -16,9 +19,20 @@ func IsAlphaNumeric(val string) bool {
 
 // IsValidSlackWebHookURL : Check if the given value is url as well as slack web hook url
 func IsValidSlackWebHookURL(urlVal string) bool {
+	if CheckIsEmptyOrNull(urlVal) {
+		return false
+	}
 	_, err := url.ParseRequestURI(urlVal)
 	if err != nil {
 		return false
 	}
 	return true
+}
+
+// CheckIsEmptyOrNull : Check if the given value is empty or not
+func CheckIsEmptyOrNull(val string) bool {
+	if len(val) == 0 {
+		return true
+	}
+	return false
 }
