855edda4735080b97d2781b11f7272c8116c2746
razzkumar <razzkumar.dev@gmail.com>
2019-10-31 11:33:38 +0545

    Fixes s3-bucket-name invalid error

--- infrastructure/internals/terraform/terraform.go
+++ infrastructure/internals/terraform/terraform.go
@@ -65,8 +65,8 @@ func getTerraformOutput(workspaceDir string) (string, error) {
 	cmd.Dir = workspaceDir
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
 	err := cmd.Run()
 
 	if err != nil {
